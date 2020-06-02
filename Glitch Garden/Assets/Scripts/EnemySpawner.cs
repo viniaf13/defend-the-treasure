136614dbb1cad0cf579f687a9693d75ca22bb131
viniaf13 <viniaf13@gmail.com>
2020-06-02 16:20:08 -0300

    added enemy spawner

---
+++ Glitch Garden/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] Attacker enemyPrefab = default;
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 5f;
+
+    private bool keepSpawning = true;
+    private IEnumerator Start()
+    {
+        while (keepSpawning)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
