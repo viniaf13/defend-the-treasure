883b2771631a74e1600f1c251d4be3ab033cc34f
viniaf13 <viniaf13@gmail.com>
2020-06-02 17:29:15 -0300

    remade usopp: idle and shoot anim

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f,2f)][SerializeField] float walkSpeed = 1f;
+    [Range(0f,2f)][SerializeField] float moveSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,11 @@ public class Attacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * walkSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float newSpeed)
+    {
+        moveSpeed = newSpeed;
     }
 }
