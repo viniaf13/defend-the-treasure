4edba4e6736732aec2c20e353e1420ae73d6b07f
viniaf13 <viniaf13@gmail.com>
2020-06-03 14:17:37 -0300

    added usopp projectile

---
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defender : MonoBehaviour
+{
+    [SerializeField] GameObject projectile = default;
+    [SerializeField] GameObject gun = default;
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, Quaternion.identity);
+    }
+}
