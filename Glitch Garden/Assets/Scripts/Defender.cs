f70d26b45d208c55fe77ee1ef748b55277c3bf98
viniaf13 <viniaf13@gmail.com>
2020-06-08 18:28:33 -0300

    defenders now attack only when there are enemies in lane

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -6,11 +6,57 @@ public class Defender : MonoBehaviour
 {
     [SerializeField] GameObject projectile = default;
     [SerializeField] GameObject firePos = default;
-
     [SerializeField] int resourceCost = 10;
+    [SerializeField] bool isResourceGenerator = false;
+
+
+    private EnemySpawner laneSpawner = default;
+    Animator anim = default;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isResourceGenerator) return;
+
+        if (IsEnemyInLane())
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("isAttacking", false);
+        }
+        
+    }
+
     public void Fire()
     {
-        Instantiate(projectile, firePos.transform.position, Quaternion.identity);
+        GameObject newProj = Instantiate
+            (projectile, firePos.transform.position, Quaternion.identity) as GameObject;
+        newProj.transform.parent = transform;
+    }
+
+    private bool IsEnemyInLane()
+    {
+        return laneSpawner.transform.childCount > 0;
+    }
+
+    private void SetLaneSpawner()
+    {
+        EnemySpawner[] spawners = FindObjectsOfType<EnemySpawner>();
+
+        foreach (EnemySpawner spawner in spawners)
+        {
+            if (transform.position.y == spawner.transform.position.y)
+            {
+                laneSpawner = spawner;
+            }
+        }
     }
 
     public int GetCost()
