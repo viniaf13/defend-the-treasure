070b2bc632702488c8bb9556b866227ca400028e
viniaf13 <viniaf13@gmail.com>
2020-06-15 18:29:25 -0300

    created a level timer slider

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] float levelTime = 10f;
+
+    private Slider slider = default;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Update()
+    {
+        slider.value = Time.timeSinceLevelLoad / levelTime;
+
+        if (slider.value == 1)
+        {
+            Debug.Log("Level finished: " + slider.value);
+        }
+    }
+}
