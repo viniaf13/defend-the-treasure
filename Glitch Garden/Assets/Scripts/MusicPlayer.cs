32783b14ec2595bac521a5afc6ff4c1e2bc863af
viniaf13 <viniaf13@gmail.com>
2020-06-29 14:20:27 -0300

    hooked up volume functionality to options menu

---
+++ Glitch Garden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    private void SetUpSingleton()
+    {
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/OptionsController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider = default;
+    [SerializeField] float defaultVolume = 0.5f;
+    [SerializeField] Slider difficultySlider = default;
+    [SerializeField] float defaultDifficulty = 1f;
+
+    MusicPlayer musicPlayer;
+
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+    }
+
+    void Update()
+    {
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found");
+        }
+    }
+
+    public void SaveOptionPrefs()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
