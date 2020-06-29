6bfdd7afd33223f4de01eae6c99df9d60647c4f2
viniaf13 <viniaf13@gmail.com>
2020-06-29 19:29:41 -0300

    added cost display for defenders

--- Glitch Garden/Assets/Scripts/DefenderButton.cs
+++ Glitch Garden/Assets/Scripts/DefenderButton.cs
@@ -1,11 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] Defender defenderPrefab = default;
     [SerializeField] Color32 unselectedColor = new Color32(87, 56, 56, 255);
+
+    private void Start()
+    {
+        CreateCostLabel();
+    }
+
+    private void CreateCostLabel()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText) { Debug.LogError(name + " has no cost text!"); }
+        costText.text = defenderPrefab.GetCost().ToString();
+    }
 
     //Highlight the defender selected ans sets it on defender spawner
     private void OnMouseDown()
